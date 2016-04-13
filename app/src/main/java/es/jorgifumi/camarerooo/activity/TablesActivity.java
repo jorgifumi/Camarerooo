659ba6acdcfb0de82e825512fc4e29102953cb4e
Jorge Lucena <jorgifumi@gmail.com>
2016-04-13 20:00:49 +0200

    Dish Model

--- app/src/main/java/es/jorgifumi/camarerooo/activity/TablesActivity.java
+++ app/src/main/java/es/jorgifumi/camarerooo/activity/TablesActivity.java
@@ -1,9 +1,12 @@
-package es.jorgifumi.camarerooo;
+package es.jorgifumi.camarerooo.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import es.jorgifumi.camarerooo.R;
+
 public class TablesActivity extends AppCompatActivity {
+    private static final String TAG = "TablesActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/es/jorgifumi/camarerooo/model/Dish.java
@@ -0,0 +1,71 @@
+package es.jorgifumi.camarerooo.model;
+
+import java.util.Set;
+
+/**
+ * Created by jorgifumi on 13/04/16.
+ */
+public class Dish {
+    private String mName;
+    private String mDescription;
+    private Set<Allergen> mAllergens;
+    private String mImageURL;
+    private String mNotes;
+
+    public Dish(String name, String description, Set<Allergen> allergens, String imageURL, String notes) {
+        mName = name;
+        mDescription = description;
+        mAllergens = allergens;
+        mImageURL = imageURL;
+        mNotes = notes;
+    }
+
+    public Dish(String name, String description, Set<Allergen> allergens, String imageURL) {
+        mName = name;
+        mDescription = description;
+        mAllergens = allergens;
+        mImageURL = imageURL;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getDescription() {
+        return mDescription;
+    }
+
+    public void setDescription(String description) {
+        mDescription = description;
+    }
+
+    public Set<Allergen> getAllergens() {
+        return mAllergens;
+    }
+
+    public void setAllergens(Set<Allergen> allergens) {
+        mAllergens = allergens;
+    }
+
+    public String getImageURL() {
+        return mImageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        mImageURL = imageURL;
+    }
+
+    public String getNotes() {
+        return mNotes;
+    }
+
+    public void setNotes(String notes) {
+        mNotes = notes;
+    }
+}
+
+enum Allergen { WHEAT, CRUSTACEANS, EGGS, FISH, PEANUTS, MILK, SOYA }
